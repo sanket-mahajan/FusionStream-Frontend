20f372d9bc4c827e52f29f27a592fd899dc67a8b
Sanket <sanketmahajan2003@gmail.com>
2025-02-06 12:38:56 +0530

    fix: change onKeyPress to onKeyDown for search input in Navbar component

--- src/components/layout/common/Navbar.jsx
+++ src/components/layout/common/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
                 placeholder="Search videos..."
                 className="rounded-md pl-4 pr-10 py-2 text-gray-900 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 onChange={(e) => setSearch(e.target.value)}
-                onKeyPress={handleKeyPress}
+                onKeyDown={handleKeyPress}
                 aria-label="Search videos"
               />
               <button
