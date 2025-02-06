cca11830d9f4e439e33fdc3fa5389f067e24f508
Sanket <sanketmahajan2003@gmail.com>
2025-02-06 12:40:59 +0530

    fix: update sidebar state management and layout responsiveness in Layout component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,24 +4,24 @@ import Footer from "./common/Footer";
 import Sidebar from "./common/Sidebar";
 
 const Layout = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
-      <Sidebar sideBarOpen={setIsSidebarOpen} />
+      <Sidebar sideBarOpen={isSidebarOpen} setSideBarOpen={setIsSidebarOpen} />
 
       {/* Main Content Area */}
       <div
-        className={`flex flex-col flex-grow min-h-screen transition-all duration-300 ${
-          isSidebarOpen ? "ml-64" : "ml-20"
+        className={`flex flex-col flex-grow min-h-screen transition-all duration-200 ${
+          isSidebarOpen ? "ml-64" : "ml-0 md:ml-20"
         }`}
       >
         {/* Navbar */}
-        <Navbar />
+        <Navbar setIsSidebarOpen={setIsSidebarOpen} />
 
         {/* Main Content Wrapper */}
-        <main className="flex-grow ">{children}</main>
+        <main className="flex-grow">{children}</main>
 
         {/* Footer at the bottom */}
         <Footer />
