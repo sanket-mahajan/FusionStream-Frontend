15a81141b353f607bf7fbcc8eda724dd2aef478e
Sanket <sanketmahajan2003@gmail.com>
2025-02-06 11:07:53 +0530

    update axios instance to use VITE_API_URL from environment variables

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import ErrorHandler from "../components/ErrorHandler/ErrorHandler";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // Sends cookies along with requests
 });
 
