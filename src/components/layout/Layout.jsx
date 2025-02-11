c0dcf344cb9029418376a6ce9487f4fc0642db4f
Sanket <sanketmahajan2003@gmail.com>
2025-02-11 18:29:14 +0530

    refactor: clean up Layout and VideoDetails components by removing commented code

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,36 +1,3 @@
-// import Navbar from "./common/Navbar";
-// import Footer from "./common/Footer";
-// import Sidebar from "./common/Sidebar";
-// import { useSelector } from "react-redux";
-
-// const Layout = ({ children }) => {
-//   const { isSidebarOpen } = useSelector((state) => state.sidebar);
-
-//   return (
-//     <div className="flex min-h-screen flex-col">
-//       <Navbar />
-
-//       <div className="flex flex-grow">
-//         <Sidebar />
-
-//         {/* Main Content Area */}
-//         <div
-//           className={`flex flex-col flex-grow min-h-screen transition-all duration-300 ${
-//             isSidebarOpen ? "ml-0 md:ml-64" : "ml-0 md:ml-20"
-//           }`}
-//         >
-//           {/* Main Content Wrapper */}
-//           <main className="flex-grow mt-16">{children}</main>
-
-//           <Footer />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Layout;
-
 import Navbar from "./common/Navbar";
 import Footer from "./common/Footer";
 import Sidebar from "./common/Sidebar";
